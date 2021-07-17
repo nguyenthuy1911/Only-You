2872334a2ddf699ec8523717d6be46d8695bc16f
nguyenthuy <tn262995@gmail.com>
2021-07-17 17:50:18 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Cậu ahhhhh!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào trong tim lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Hi cậu',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😍',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Trên đời này có kẻ mạnh người yếu, còn tớ với cậu là "Nguoi yeu"',
+    btnNo: 'Đừng hỗn với tớ, tớ "hon lai day"',
+    question:'Thế giới có 7 tỉ người, chỉ cần thấy cậu là tớ SEE tình',
+    btnReply: 'Why :3 ',
+    reply: 'Một từ thôi không đủ, vì thích cậu ngàn thu không đổi <33333333',
+    mess: 'Covid thì tớ không dính, nhưng thích cậu thì tớ dương tính ',
+    messDesc: 'Này chàng trai tớ mê, nếu cậu không bận IBê tớ này',
+    btnAccept: 'Đang nghĩ về cậu đấy <3',
+    messLink: 'https://www.facebook.com/Aippi1021611040206'
+}
